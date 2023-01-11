a80540addfaf60a00cf34c1c489b1cd5e3781700
Sergey Shappo <shappoff@gmail.com>
2023-01-11 13:11:13 +0300

    split names by sex

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,41 @@ import {names} from "./namse";
 
 const App = () => {
     const [value, setValue] = React.useState<string>('');
+    const [currentSex, setCurrentSex] = React.useState<'male' | 'female'>('male');
     const [hits, setHits] = React.useState<Array<any>>([]);
     React.useEffect(() => {
         if (value.length) {
-            setHits(names.filter(({name}: any) => {
+            setHits(names.filter(({name, sex}: any) => {
                 const regexp = new RegExp(value, "g");
-                return name.match(regexp);
+                return name.match(regexp) && currentSex === sex;
             }));
         } else {
             setHits([]);
         }
-    }, [value]);
-    const searchHandler = ({target}: any) => {
-        setValue(target.value);
-    }
+    }, [value, currentSex]);
+
+    const searchHandler = ({target}: any) => setValue(target.value);
+
+    const radioHandler = ({currentTarget}: any) => setCurrentSex(currentTarget.value);
 
     return <div>
+        <div className="type-table">
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="type" id="male" value="male" defaultChecked onChange={radioHandler} />
+                    <label className="form-check-label" htmlFor="male">Мужское</label></div>
+            <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="type" id="female" value="female"  onChange={radioHandler} />
+                    <label className="form-check-label" htmlFor="female">Женское</label></div>
+        </div>
         <input autoFocus onChange={searchHandler} type="text" value={value} id="input"/>
         <ul className="list-group">
             {
-                hits.map(({name, sex}, index) => <li key={index} className="list-group-item" dangerouslySetInnerHTML={{__html: `${name}, ${sex === 'male' ? 'муж' : 'жен'}`}}></li>)
+                hits.map(({name, sex}, index) =>
+                    <li key={index}
+                        className="list-group-item"
+                        dangerouslySetInnerHTML={{__html: `${name}, ${sex === 'male' ? 'муж' : 'жен'}`}}
+                    ></li>
+                )
             }
         </ul>
     </div>
